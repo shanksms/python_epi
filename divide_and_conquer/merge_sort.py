f3ac509f9de575138fc7a9eab40f7e697b3dfd15
unknown <sriv.srivastava@gmail.com>
2021-05-15 16:16:29 +0530

    check in code of merge sort

--- divide_and_conquer/merge_sort.py
+++ divide_and_conquer/merge_sort.py
@@ -1,9 +1,10 @@
 def merge(arr, l, m, r):
     n1 = m - l + 1
     n2 = r -m
-    L = [0] * (n1)
-    R = [0] * (n2)
-
+    #L = [0] * (n1)
+    #R = [0] * (n2)
+    L = [x for x in range(0, n1)]
+    R = [x for x in range(0, n2)]
     for i in range(0, n1):
         L[i] = arr[l + i]
     for j in range(0, n2):
