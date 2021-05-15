460d4d4a5915f540107b02d3b9864b474fb6a49f
unknown <sriv.srivastava@gmail.com>
2021-05-15 22:40:41 +0530

    modified merge sort

--- divide_and_conquer/merge_sort.py
+++ divide_and_conquer/merge_sort.py
@@ -1,43 +1,40 @@
-def merge(arr, l, m, r):
-    n1 = m - l + 1
-    n2 = r -m
-    #L = [0] * (n1)
-    #R = [0] * (n2)
-    L = [x for x in range(0, n1)]
-    R = [x for x in range(0, n2)]
+def merge(left, arr, mid, right):
+    n1 = mid - left + 1
+    n2 = right - mid
+    A = []
+    B = []
     for i in range(0, n1):
-        L[i] = arr[l + i]
-    for j in range(0, n2):
-        R[j] = arr[m + 1 + j]
-    i = 0
-    j = 0
-    k = l
+        A.append(arr[left + i])
+    for i in range(0, n2):
+        B.append(arr[mid + 1 + i])
+    i, j, k = 0, 0, left
     while i < n1 and j < n2:
-        if L[i] <= R[j]:
-            arr[k] = L[i]
+        if A[i] < B[j]:
+            arr[k] = A[i]
+            k += 1
             i += 1
         else:
-            arr[k] = R[j]
+            arr[k] = B[j]
+            k += 1
             j += 1
-        k += 1
-
     while i < n1:
-        arr[k] = L[i]
+        arr[k] = A[i]
+        k += 1
         i += 1
-        k += 1
     while j < n2:
-        arr[k] = R[j]
-        j += 1
+        arr[k] = B[j]
         k += 1
+        j += 1
 
 
 
-def merge_sort(arr, l, r ):
-    if l < r:
-        m = (l + r) // 2
-        merge_sort(arr, l, m)
-        merge_sort(arr, m + 1, r)
-        merge(arr, l, m, r)
+
+def merge_sort(arr, left, right):
+    if left < right:
+        mid = (left + right) // 2
+        merge_sort(arr, left, mid)
+        merge_sort(arr, mid + 1, right)
+        merge(left, arr, mid, right)
 
 
 if __name__ == '__main__':
