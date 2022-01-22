2d9bd491919fbc9e946ca20b1710531d8f367330
shanksms <sriv.srivastava@gmail.com>
2022-01-22 22:04:11 +0530

    binary search

--- searching/binary_search.py
+++ searching/binary_search.py
@@ -1,25 +1,25 @@
 from typing import List
 
-"""
-Algorithm:
-_input: List
- target: int
- start = 0
- end = len(_input) - 1
-1. Loop until start < end
-    
-    M = start + (end - start) / 2
-    if  _input[M] == target:
-        return M
-    elif _input[M] > target:
-        end = M - 1
-    else:
-        start = M +1
 
- 
-"""
+def binary_search(_input: List, target: int):
+    """
+    Algorithm:
+    _input: List
+     target: int
+     start = 0
+     end = len(_input) - 1
+    1. Loop until start < end
 
-def search(_input: List, target: int):
+        M = start + (end - start) / 2
+        if  _input[M] == target:
+            return M
+        elif _input[M] > target:
+            end = M - 1
+        else:
+            start = M +1
+
+
+    """
     start, end = 0, len(_input) - 1
     while start < end:
         M = int(start + (end - start) / 2)
@@ -32,7 +32,45 @@ def search(_input: List, target: int):
     return -1
 
 
-if __name__ == '__main__':
-    assert search([1, 2, 3, 4, 5], 3) == 2
-    assert search([1, 2, 3, 5], 10) == -1
+def find_first_occurrence(sorted_list: List, target: int) -> int:
+    """
+    Algorithm
+    input --> list
+    target --> int
+    low, high, result = 0, len(input), -1
+    while low <= high:
+        mid = int(low + (high - low) / 2)
+        if input[mid] > target:
+            high = mid - 1
+        elif input[mid] == target:
+            result = mid
+            high = mid - 1
+        else:
+            low = mid + 1
+    return result
+    """
+    low, high, result = 0, len(sorted_list), -1
+    while low <= high:
+        mid = int(low + (high - low) / 2)
+        if sorted_list[mid] > target:
+            high = mid - 1
+        elif sorted_list[mid] == target:
+            result = mid
+            high = mid - 1
+        else:
+            low = mid + 1
+    return result
 
+
+def first_occurence_of_element_greater_than_key(sorted_list: List, target: int) -> int:
+    low, high, result = 0, len(sorted_list), -1
+    while low <= high:
+        mid = int(low + (high - low) / 2)
+        if sorted_list[mid] > target:
+            high = mid - 1
+            result = mid
+        elif sorted_list[mid] == target:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return result
--- searching/binary_search_test.py
+++ searching/binary_search_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from searching.binary_search import find_first_occurrence
+from searching import binary_search
 
 
 @pytest.mark.parametrize('sorted_input_list, target, result',
@@ -11,4 +11,15 @@ from searching.binary_search import find_first_occurrence
 
 )
 def test_find_first_occurrence(sorted_input_list, target, result):
-    assert find_first_occurrence(sorted_input_list, target) == result
+    assert binary_search.find_first_occurrence(sorted_input_list, target) == result
+
+
+@pytest.mark.parametrize('sorted_input_list, target, result',
+                         [
+                             ([1, 2, 3, 4, 5], 2, 2),
+                             ([-14, -10, 2, 108, 108, 243], -13, 1),
+                         ]
+
+)
+def test_first_occurence_of_element_greater_than_key(sorted_input_list, target, result):
+    assert binary_search.first_occurence_of_element_greater_than_key(sorted_input_list, target) == result
