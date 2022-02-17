a65f6ceae0ecab6440e2d0389df7a7ceee376fcd
shanksms <sriv.srivastava@gmail.com>
2022-02-17 20:01:25 +0530

    improved indentation

--- arrays/two_sums.py
+++ arrays/two_sums.py
@@ -60,6 +60,8 @@ def twoSum_approach2(self, nums: List[int], target: int) -> List[int]:
     num_dict = dict()
     for i, num in enumerate(nums):
         if (target - num) in num_dict:
-            return [i, num_dict[(target - num)]]
+            return [
+                i, num_dict[(target - num)]
+            ]
         num_dict[num] = i
 
