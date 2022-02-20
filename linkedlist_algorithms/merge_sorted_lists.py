5010478da18d0ed93f3ec6b048916faa8e663ef8
shanksms <sriv.srivastava@gmail.com>
2022-02-20 13:50:38 +0530

    merge sorted lists

--- linkedlist_algorithms/merge_sorted_lists.py
+++ linkedlist_algorithms/merge_sorted_lists.py
@@ -1,12 +1,27 @@
+from typing import Optional, List
 from linkedlist_algorithms.linkedlist import ListNode
-def merge_sorted_lists(l1, l2):
-    dummy_head = tail = ListNode(0, None)
-    while l1 and l2:
-        if l1.data > l2.data:
-            tail.next, l2 = l2, l2.next
-        else:
-            tail.next, l1 = l1, l1.next
-        tail = tail.next
-    tail.next = l1 or l2
-    return dummy_head.next
+import heapq
+
+
+def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    # second condition is to check empty sublists
+    if not lists or not any(lists):
+        return None
+    min_heap = []
+    for _idx, llist in enumerate(lists):
+        if llist:
+            heapq.heappush(min_heap, (llist.val, _idx))
+            lists[_idx] = llist.next
+
+    dummy_node = ListNode()
+    tail = dummy_node
+    while len(min_heap) > 0:
+        val, _idx = heapq.heappop(min_heap)
+        node = ListNode(val)
+        tail.next, tail = node, node
+        if lists[_idx]:
+            heapq.heappush(min_heap, (lists[_idx].val, _idx))
+            lists[_idx] = lists[_idx].next
+
+    return dummy_node.next
 
