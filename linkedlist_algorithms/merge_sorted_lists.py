3c360a06ab7ab07af420adf69e83782497c3a291
shanksms <sriv.srivastava@gmail.com>
2022-02-20 16:34:30 +0530

    merge sorted lists

--- linkedlist_algorithms/merge_sorted_lists.py
+++ linkedlist_algorithms/merge_sorted_lists.py
@@ -3,7 +3,18 @@ from linkedlist_algorithms.linkedlist import ListNode
 import heapq
 
 
+
 def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    """
+    Algorithm:
+    There are two key points here:
+    1. Use of heap to get the minimum value stored at any given point
+    2. store indexes of the child lists in the parent list
+
+    :param self:
+    :param lists:
+    :return:
+    """
     # second condition is to check empty sublists
     if not lists or not any(lists):
         return None
